909ad98cc22e3342bb268ff7ef223ceb202946b6
AbdulRehmanHashmi <155216102+AbdulRehmanHashmi@users.noreply.github.com>
2024-05-16 02:52:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var main_img = document.getElementById('main_img')
+
+var image1 = document.getElementById('image1')
+image1.addEventListener('mouseover' , function(){
+    main_img.src = 'image 2.jpeg'
+})
+
+image1.addEventListener('mouseout' , function(){
+    main_img.src = 'image 1.jpeg'
+})
+
+image2.addEventListener('mouseover' , function(){
+    main_img.src = 'image 3.jpeg'
+})
+
+image2.addEventListener('mouseout' , function(){
+    main_img.src = 'image 1.jpeg'
+})
+
+image3.addEventListener('mouseover' , function(){
+    main_img.src = 'image 4.jpeg'
+})
+
+image3.addEventListener('mouseout' , function(){
+    main_img.src = 'image 1.jpeg'
+})
+
+image4.addEventListener('mouseover' , function(){
+    main_img.src = 'image 5.jpeg'
+})
+
+image4.addEventListener('mouseout' , function(){
+    main_img.src = 'image 1.jpeg'
+})
+
+image5.addEventListener('mouseover' , function(){
+    main_img.src = 'image 6.jpeg'
+})
+
+image5.addEventListener('mouseout' , function(){
+    main_img.src = 'image 1.jpeg'
+})
